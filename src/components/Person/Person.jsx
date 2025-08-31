adddb4267ab642cdb4841795c9ac3c7c66ee4c8d
dimon <80713na@gmail.com>
2025-09-01 02:49:31 +0300

    peson

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,20 +1,19 @@
-export const Person = ({person}) => {
-const sex = person.sex === 'm' ? 'wifi' : 'husband'
+export const Person = ({ person }) => {
+  const sex = person.sex === 'm' ? 'wifi' : 'husband';
 
- return ( 
-  
-  <section className="Person">
-  <h2 className="Person__name">My name is {person.name}</h2>
-  
-  {person.age ? (
-    <p className="Person__age">I am {person.age}</p> )
-    : ' '
-  }
-  
-  {person.isMarried ?(
-    <p className="Person__partner">{person.partnerName} is my {sex}</p>)
-    :<p className="Person__partner">I am not married</p>
-  }
-  </section>
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+
+      {person.age ? <p className="Person__age">I am {person.age}</p> : ' '}
+
+      {person.isMarried ? (
+        <p className="Person__partner">
+          {person.partnerName} is my {sex}
+        </p>
+      ) : (
+        <p className="Person__partner">I am not married</p>
+      )}
+    </section>
   );
-}
+};
