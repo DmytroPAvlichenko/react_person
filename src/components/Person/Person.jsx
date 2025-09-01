8dd08384342b22e6c573cbd5887d3c377f762433
dimon <80713na@gmail.com>
2025-09-01 16:32:51 +0300

    peson

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,15 +1,17 @@
-export const Person = ({ person }) => {
-  const sex = person.sex === 'm' ? 'wife' : 'husband';
+export const Person = ({person})  => {
+  const { name, age, sex, isMarried, partnerName } = person;
+  
+  const mery = sex === 'm' ? 'wife' : 'husband';
 
   return (
     <section className="Person">
-      <h2 className="Person__name">My name is {person.name}</h2>
+      <h2 className="Person__name">My name is {name}</h2>
 
-      {person.age ? <p className="Person__age">I am {person.age}</p> : ' '}
+      {age ? <p className="Person__age">I am {age}</p> : ' '}
 
-      {person.isMarried ? (
+      {isMarried ? (
         <p className="Person__partner">
-          {person.partnerName} is my {sex}
+          {partnerName} is my {mery}
         </p>
       ) : (
         <p className="Person__partner">I am not married</p>
