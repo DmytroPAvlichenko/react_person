f420fa2bbd525fd3880dfa4d03f8a55cf62d8778
dimon <80713na@gmail.com>
2025-09-01 16:34:33 +0300

    peson

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,6 +1,6 @@
-export const Person = ({person})  => {
+export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
-  
+
   const mery = sex === 'm' ? 'wife' : 'husband';
 
   return (
